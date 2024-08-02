78575eeab1a0140be882e4d6c4b03fb60f1d35ea
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-08-02 14:38:44 +1000

    test code (function) to try and debug why one of the test cases doesn't work...

--- src/caesarCipher/index.js
+++ src/caesarCipher/index.js
@@ -2,25 +2,36 @@ const caesarCipher = (str, shiftFactor) => {
   let res = "";
   const LETTERS_IN_ALPHABET = 26;
   const isLetter = (character) =>
-    character.toLowerCase() != character.toUpperCase();
+    character.toLowerCase() !== character.toUpperCase();
 
   // helper
   const shiftLetter = (letter) => {
     const code = letter.charCodeAt(0);
+    console.log("\nletter:", letter);
+    console.log("code:", code);
     const isUpperCase = (letter) => letter === letter.toUpperCase();
 
-    if (isUpperCase)
-      return String.fromCharCode(((code - 97 + shiftFactor) % 26) + 97);
-    return String.fromCharCode(((code - 65 + shiftFactor) % 26) + 65);
+    if (isUpperCase) {
+      console.log("code - 97 -> CODE", code - 97);
+      console.log("add shift factor CODE", code - 97 + shiftFactor);
+      console.log("modulo 26 CODE", (code - 97 + shiftFactor) % 26);
+      console.log("final code", ((code - 97 + shiftFactor) % 26) + 97);
+      return String.fromCharCode(((code - 97 + shiftFactor) % LETTERS_IN_ALPHABET) + 97);
+    }
+    return String.fromCharCode(((code - 65 + shiftFactor) % LETTERS_IN_ALPHABET) + 65);
   };
 
   for (let i = 0; i < str.length; i++) {
     let char = str[i];
+    // console.log(char);
     if (isLetter(char)) char = shiftLetter(char);
+    console.log(char);
     res += char;
   }
 
   return res;
 };
 
-export default caesarCipher;
+// export default caesarCipher;
+
+caesarCipher("HENRY", 10);
