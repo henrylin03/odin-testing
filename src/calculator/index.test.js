94b96fdb31c4d13af37b1355de3ca9619e6abe59
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-08-02 11:01:37 +1000

    add test for subtraction method of calculator, including one for decimals

--- src/calculator/index.test.js
+++ src/calculator/index.test.js
@@ -8,3 +8,12 @@ describe("Check numbers are correctly added", () => {
     expect(calculator.add(0.2, 0.4)).toBeCloseTo(0.6); // avoids rounding error
   });
 });
+
+describe("Check numbers are correctly subtracted", () => {
+  test("Three minus one is two", () => {
+    expect(calculator.subtract(3, 2)).toBe(1);
+  });
+  test("Minusing decimals", () => {
+    expect(calculator.subtract(0.9, 0.8)).toBeCloseTo(0.1); // avoids rounding error
+  });
+});
