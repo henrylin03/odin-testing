029cece4b5b247caa8396bc69c439b8940e6773b
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-08-02 10:47:52 +1000

    write test cases to confirm that string has reversed. also check palindromes actually just stay the same

---
+++ src/reverseString/index.test.js
@@ -0,0 +1,10 @@
+import reverseString from ".";
+
+describe("Check string has reversed", () => {
+  test("String has reversed", () => {
+    expect(reverseString("hello")).toBe("olleh");
+  });
+  test("Palindrome stays the same", () => {
+    expect(reverseString("racecar")).toBe("racecar");
+  });
+});
