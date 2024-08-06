4228770f923310ca9ea765eb668d8d6523919626
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-08-06 15:48:30 +1000

    make clear that the reason we are modulo 26 is because that's the number of letters in the alphabet

--- src/caesarCipher/index.js
+++ src/caesarCipher/index.js
@@ -1,5 +1,7 @@
 const caesarCipher = (str, shift) => {
   let res = "";
+  const LETTERS_IN_ALPHABET = 26;
+
   [...str].forEach((char) => {
     const isLetter = char.toLowerCase() !== char.toUpperCase();
     if (!isLetter) return (res += char);
@@ -8,7 +10,8 @@ const caesarCipher = (str, shift) => {
     const isUpperCase = char === char.toUpperCase();
     const codeForLetterA = isUpperCase ? 65 : 97;
 
-    const shiftedCode = ((code - codeForLetterA + shift) % 26) + codeForLetterA;
+    const shiftedCode =
+      ((code - codeForLetterA + shift) % LETTERS_IN_ALPHABET) + codeForLetterA;
 
     res += String.fromCharCode(shiftedCode);
   });
